df51eef0af621580a0749a49ba38701c88adfdd4
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-02-13 19:54:15 +0800

    Add contexts and urls

---
+++ recipebook/ledger/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import recipe_list, recipe_one, recipe_two
+
+urlpatterns = [
+    path('recipes/list', recipe_list, name='recipe_list'),
+    path('recipe/1', recipe_one, name="recipe_one"),
+    path('recipe/2', recipe_two, name='recipe_two')
+]
+
+app_name = 'ledger'
