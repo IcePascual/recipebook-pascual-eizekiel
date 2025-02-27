fd7e531fd39dc64ff4f42ac3aa404070865d9f2c
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-02-27 19:31:57 +0800

    Remove obsolute imports

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView, ingredient
+from .views import RecipeListView, RecipeDetailView
 
 urlpatterns = [
     path('recipes/list', RecipeListView.as_view(), name='recipe_list'),
