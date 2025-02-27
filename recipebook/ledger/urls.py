27d598e9374b75d09e422899eadd7e7f6afcd181
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-02-27 19:31:39 +0800

    Remove obsolute urlpattern

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -4,7 +4,6 @@ from .views import RecipeListView, RecipeDetailView, ingredient
 urlpatterns = [
     path('recipes/list', RecipeListView.as_view(), name='recipe_list'),
     path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe'),
-    path('ingredient/<int:pk>', ingredient, name='ingredient')
 ]
 
 app_name = 'ledger'
