0604cac4e460eeecebf390f43465899ac4d3d453
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-02-27 19:06:51 +0800

    Convert recipe page to DetailView

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Recipe, Ingredient
 
 # Create your views here.
 class RecipeListView(ListView):
     model = Recipe
     template_name = 'ledger/recipe_list.html'
+
+class RecipeDetailView(DetailView):
+    model = Recipe
+    template_name = 'ledger/recipe.html'
 
 
 def recipe(request, pk):
