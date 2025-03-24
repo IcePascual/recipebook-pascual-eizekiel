ae9848652799e2bff4412695a20055c3954177cf
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-03-24 23:46:07 +0800

    Add image adding functionality to recipes

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import RecipeListView, RecipeDetailView, RecipeCreateView
+from .views import RecipeListView, RecipeDetailView, RecipeCreateView, RecipeAddImageView
 
 urlpatterns = [
     path('recipes/list', RecipeListView.as_view(), name='recipe_list'),
     path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe'),
+    path('recipe/<int:pk>/add_image', RecipeAddImageView.as_view(), name='recipe_add_image'),
     path('recipe/add', RecipeCreateView.as_view(), name='recipe_create'),
 ]
 
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,9 +1,10 @@
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
-from .models import Recipe
-from .forms import RecipeForm
+from .models import Recipe, RecipeImage
+from .forms import RecipeForm, RecipeImageForm
 
 
 # Create your views here.
@@ -22,3 +23,16 @@ class RecipeCreateView(LoginRequiredMixin, CreateView):
     model = Recipe
     form_class = RecipeForm
     template_name = 'ledger/recipe_create.html'
+
+
+class RecipeAddImageView(CreateView):
+    model = RecipeImage
+    form_class = RecipeImageForm
+    template_name = 'ledger/recipe_add_image.html'
+    
+    def form_valid(self, form):
+        form.instance.recipe = Recipe.objects.get(pk=self.kwargs['pk'])
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse_lazy('ledger:recipe', kwargs={ 'pk': self.kwargs['pk'] })
