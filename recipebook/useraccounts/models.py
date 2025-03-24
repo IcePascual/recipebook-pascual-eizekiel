2961338c14e412e65a5a64ca1869a64e5a6e42d0
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-03-24 14:15:58 +0800

    Remove 255 character limit on bio

--- recipebook/useraccounts/models.py
+++ recipebook/useraccounts/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     username = models.CharField(max_length=50)
-    bio = models.TextField(max_length=255)
+    bio = models.TextField()
 
     def __str__(self):
         return self.username
