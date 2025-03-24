ceb9af53256e78ff107a569114977a315b00af93
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-03-24 20:13:32 +0800

    Adjust handling of settings constants and urls

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = '/recipes/list'
 LOGOUT_REDIRECT_URL = '/accounts/login'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
+MEDIA_ROOT = BASE_DIR/'media'
 MEDIA_URL = '/media/'
 
 # Default primary key field type
--- recipebook/recipebook/urls.py
+++ recipebook/recipebook/urls.py
@@ -19,10 +19,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .settings import MEDIA_URL, MEDIA_ROOT
+
 urlpatterns = [
     path('', include("ledger.urls", namespace="ledger")),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
