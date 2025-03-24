114a0e1e432b5e9a9af7f2abad41ea97fd99133f
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-03-24 19:09:09 +0800

    Add media functionality

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -15,6 +15,7 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,6 +126,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = '/recipes/list'
 LOGOUT_REDIRECT_URL = '/accounts/login'
+MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
