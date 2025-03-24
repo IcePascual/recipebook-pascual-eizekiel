0b3e5325ecac09030adb7c11927c02083c3a56b2
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-03-24 21:28:06 +0800

    Add recipe creation

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,7 +1,9 @@
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
 
 from .models import Recipe
+from .forms import RecipeForm
 
 
 # Create your views here.
@@ -14,3 +16,9 @@ class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'ledger/recipe.html'
     redirect_field_name = 'recipe_list'
+
+
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    form_class = RecipeForm
+    template_name = 'ledger/recipe_create.html'
