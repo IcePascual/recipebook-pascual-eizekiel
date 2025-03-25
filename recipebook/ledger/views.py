cb2d5bbf7ef319cf9883c49cbf260192b1d8fc7a
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-03-25 10:41:32 +0800

    Fix return link not working

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -30,9 +30,14 @@ class RecipeAddImageView(CreateView):
     form_class = RecipeImageForm
     template_name = 'ledger/recipe_add_image.html'
 
-    def form_valid(self, form):
-        form.instance.recipe = Recipe.objects.get(pk=self.kwargs['pk'])
-        return super().form_valid(form)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['pk'] = self.kwargs['pk']
+        return context
 
     def get_success_url(self):
         return reverse_lazy('ledger:recipe', kwargs={'pk': self.kwargs['pk']})
+
+    def form_valid(self, form):
+        form.instance.recipe = Recipe.objects.get(pk=self.kwargs['pk'])
+        return super().form_valid(form)
